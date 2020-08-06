c109ae5e27098a5bb913f88bd8c5f5da1f90e9aa
Samuel Tran <tran.ksamuel@gmail.com>
2020-08-05 19:34:40 -0500

    Add Golden Master

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -8,10 +8,10 @@ namespace csharpcore
         [Fact]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("foo", Items[0].Name); 
+            Assert.Equal("Conjured", Items[0].Name); 
         }
     }
 }
